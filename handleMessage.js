88f522a1ade419640d6d885c1b813830b764915e
John McLear <john@mclear.co.uk>
2020-11-22 15:04:06 +0000

    autofixes from Etherpad checkPlugins.js

--- handleMessage.js
+++ handleMessage.js
@@ -1,19 +1,19 @@
-var crypto = require('crypto'),
-padManager = require("ep_etherpad-lite/node/db/PadManager"),
-        fs = require('fs'),
-    md5sum = crypto.createHash('md5');
+const crypto = require('crypto');
+const padManager = require('ep_etherpad-lite/node/db/PadManager');
+const fs = require('fs');
+const md5sum = crypto.createHash('md5');
 
-exports.handleMessage = function(hook_name, context, callback){
-  if (context.message && context.message.data){
-    if (context.message.data.type == 'WRITE_TO_FILESYSTEM' ) { // if it's a request to update an authors email
+exports.handleMessage = function (hook_name, context, callback) {
+  if (context.message && context.message.data) {
+    if (context.message.data.type == 'WRITE_TO_FILESYSTEM') { // if it's a request to update an authors email
       // Write padId data to filesystem
-      padManager.getPad(context.message.data.padId, null, function(err, value){
-        var hash = crypto.createHash('md5').update(context.message.data.padId).digest("hex");
-        fs.writeFile("/tmp/"+hash, value.atext.text, function(err) {
-          if(err){
-            console.err("failed to write text to hashed location");
-          }else{
-            console.log("Wrote pad contents to /tmp/"+hash);
+      padManager.getPad(context.message.data.padId, null, (err, value) => {
+        const hash = crypto.createHash('md5').update(context.message.data.padId).digest('hex');
+        fs.writeFile(`/tmp/${hash}`, value.atext.text, (err) => {
+          if (err) {
+            console.err('failed to write text to hashed location');
+          } else {
+            console.log(`Wrote pad contents to /tmp/${hash}`);
             // everything went great, we could tell the client here if we wanted..
           }
         });
@@ -22,4 +22,4 @@ exports.handleMessage = function(hook_name, context, callback){
     }
   }
   callback();
-}
+};
--- static/js/add_buttons.js
+++ static/js/add_buttons.js
@@ -1,22 +1,22 @@
-/*** 
+/** *
 *
 * Most of the logic for task lists are done here
 *
 */
 
-if(typeof exports == 'undefined'){
-  var exports = this['mymodule'] = {};
+if (typeof exports === 'undefined') {
+  var exports = this.mymodule = {};
 }
 
 exports.add_buttons = {
-  /***
+  /** *
   *
   *  Add button to the editbar and bind a listener
   *
   ***/
 
-  init: function(){ // Write the button to the dom
-    var spacer = '<li class="acl-write separator"></li>';
+  init() { // Write the button to the dom
+    const spacer = '<li class="acl-write separator"></li>';
     var buttonHTML = '<li class="acl-write" id="button1"><a class="grouped-left" data-l10n-id="pad.toolbar.add_buttons.title" title="Task list Checkbox"><span class="buttonicon buttonicon-add_buttons"></span></a></li>';
     $('.menu_left').append(spacer);
     $('.menu_left').append(buttonHTML);
@@ -24,23 +24,22 @@ exports.add_buttons = {
     var buttonHTML = '<li class="acl-write" id="button2"><a class="grouped-right" data-l10n-id="pad.toolbar.add_buttons.title" title="Task list Checkbox"><span class="buttonicon buttonicon-add_buttons"></span></a></li>';
     $('.menu_left').append(buttonHTML);
 
-    $('#button1, #button2').click(function(){
+    $('#button1, #button2').click(() => {
       exports.add_buttons.onButton();
     });
-
   },
-  onButton: function(){ // tell server to save pad content to a local text file and open a new window..
+  onButton() { // tell server to save pad content to a local text file and open a new window..
     exports.add_buttons.sendMessageToWriteTxtOfPadToFileSystem();
     // window.open(); Disabled for now.
   },
-  sendMessageToWriteTxtOfPadToFileSystem: function(){ // Tell file system to write the padId to the server filesytem
-    var message = {};
+  sendMessageToWriteTxtOfPadToFileSystem() { // Tell file system to write the padId to the server filesytem
+    const message = {};
     message.type = 'WRITE_TO_FILESYSTEM';
     message.padId = pad.getPadId();
-    if(message){
+    if (message) {
       pad.collabClient.sendMessage(message);
     }
-  }
-}
+  },
+};
 
-exports.postAceInit = function(hook, context){exports.add_buttons.init()};
+exports.postAceInit = function (hook, context) { exports.add_buttons.init(); };
